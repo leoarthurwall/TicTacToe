8ea26cc4c0d1e40f4fbbedc692b0337164950dc9
leoarthurwall <leowall93@gmail.com>
2023-01-15 23:29:58 +0000

    changed the value to either a string or an x or 0 and then display it within the grid manually

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -4,21 +4,22 @@ import { useState } from "react";
 
 const Board = () => {
   const [boxes, setBoxes] = useState([
-    { id: 0, value: 0 },
-    { id: 1, value: 0 },
-    { id: 2, value: 0 },
-    { id: 3, value: 0 },
-    { id: 4, value: 0 },
-    { id: 5, value: 0 },
-    { id: 6, value: 0 },
-    { id: 7, value: 0 },
-    { id: 8, value: 0 },
+    { id: 0, value: "" },
+    { id: 1, value: "" },
+    { id: 2, value: "" },
+    { id: 3, value: "" },
+    { id: 4, value: "" },
+    { id: 5, value: "0" },
+    { id: 6, value: "X" },
+    { id: 7, value: "" },
+    { id: 8, value: "" },
   ]);
+
   return (
     <div className={styles.container}>
       {boxes.map((box, index) => (
         <div className={styles.box} key={index} value={box.value}>
-         
+          <h2 className={styles.counter}>{box.value}</h2>
         </div>
       ))}
     </div>

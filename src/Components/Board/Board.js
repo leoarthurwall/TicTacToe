cb0b4c4dcdc821213026e11f0e9db7c5f03fb613
leoarthurwall <leowall93@gmail.com>
2023-01-15 09:23:50 +0000

    set the grid layout for the 9 boxes. now need to style accordingly

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from "./Board.module.css"
+import React from "react";
+import styles from "./Board.module.css";
 
 const Board = () => {
   return (
     <div className={styles.container}>
-    
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
+      <div className={styles.box}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;

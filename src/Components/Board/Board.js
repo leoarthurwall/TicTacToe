347276b06cc160fe6ab12847bfbfe808961ba546
leoarthurwall <leowall93@gmail.com>
2023-01-17 23:04:31 +0000

    changed code to set an array of empty string using the array fill method

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -3,51 +3,23 @@ import styles from "./Board.module.css";
 import { useState } from "react";
 
 const Board = () => {
-  const [boxes, setBoxes] = useState([
-    { id: "0", value: "" },
-    { id: "1", value: "" },
-    { id: "2", value: "" },
-    { id: "3", value: "" },
-    { id: "4", value: "" },
-    { id: "5", value: "" },
-    { id: "6", value: "" },
-    { id: "7", value: "" },
-    { id: "8", value: "" },
-  ]);
-  const [count, setCount] = useState(0);
+  const [boxes, setBoxes] = useState(Array(9).fill(""));
 
-  const handleCounterClick = (e) => {
-    const updatedValue = [...boxes].map((box) => {
-      if (box.id === e.target.id) {
-        if (count % 2 === 0) {
-          box.value = "0";
-        } else {
-          box.value = "X";
-        }
-      }
-      setCount(count + 1);
-      return box;
-    });
-    setBoxes(updatedValue);
-    console.log({ updatedValue });
-    console.log(
-      "value length",
-      boxes.value
+  const handleCounterClick = () => {
+    
+  }
 
-      // set number of boxes to 0 if
-    );
-  };
 
+  console.log({boxes})
   return (
     <div className={styles.container}>
       {boxes.map((box, index) => (
         <div
           className={styles.box}
-          id={box.id}
           key={index}
           onClick={handleCounterClick}
         >
-          <h2 className={styles.counter} id={box.id}>
+          <h2 className={styles.counter} >
             {box.value}
           </h2>
         </div>
@@ -66,4 +38,22 @@ create an array of 9 objects
 
 map through the array and display 0 (an empty box)
 
+
 */
+
+  // const handleCounterClick = (e) => {
+  //   const updatedValue = [...boxes].map((box) => {
+  //     if (box.id === e.target.id) {
+  //       if (count % 2 === 0) {
+  //         box.value = "0";
+  //       } else {
+  //         box.value = "X";
+  //       }
+  //     }
+  //     setCount(count + 1);
+  //     return box;
+  //   });
+  //   setBoxes(updatedValue);
+  // };
+
+  //  const [count, setCount] = useState(0);

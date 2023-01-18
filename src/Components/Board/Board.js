4c84b1d79dce4f41d20fb84280bc8b64bfb102d3
leoarthurwall <leowall93@gmail.com>
2023-01-18 01:01:20 +0000

    working on handle click to toggle user based on boolean

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -4,19 +4,32 @@ import { useState } from "react";
 import Counters from "../Counters/Counters";
 
 const Board = () => {
-  const [boxes, setBoxes] = useState(Array(9).fill(""));
-  const [isX, setIsX] = useState(true)
+  const [isX, setIsX] = useState(true);
+  const [boxes, setBoxes] = useState([
+    { id: 1, value: "" },
+    { id: 2, value: "" },
+    { id: 3, value: "X" },
+    { id: 4, value: "0" },
+    { id: 5, value: "" },
+    { id: 6, value: "" },
+    { id: 7, value: "" },
+    { id: 8, value: "" },
+    { id: 9, value: "" },
+  ]);
 
-  const handleCounterClick = (box) => {
-  }
-
-
-
-  console.log({boxes})
+  console.log({ boxes });
   return (
     <div className={styles.container}>
-      {boxes.map((box, index) => (
-        <Counters box={box} key={index} />
+      {boxes.map((box) => (
+        <Counters
+          box={box}
+          key={box.id}
+          value={box}
+          boxes={boxes}
+          setboxes={setBoxes}
+          isX={isX}
+          setIsX={setIsX}
+        />
       ))}
     </div>
   );
@@ -35,19 +48,19 @@ map through the array and display 0 (an empty box)
 
 */
 
-  // const handleCounterClick = (e) => {
-  //   const updatedValue = [...boxes].map((box) => {
-  //     if (box.id === e.target.id) {
-  //       if (count % 2 === 0) {
-  //         box.value = "0";
-  //       } else {
-  //         box.value = "X";
-  //       }
-  //     }
-  //     setCount(count + 1);
-  //     return box;
-  //   });
-  //   setBoxes(updatedValue);
-  // };
+// const handleCounterClick = (e) => {
+//   const updatedValue = [...boxes].map((box) => {
+//     if (box.id === e.target.id) {
+//       if (count % 2 === 0) {
+//         box.value = "0";
+//       } else {
+//         box.value = "X";
+//       }
+//     }
+//     setCount(count + 1);
+//     return box;
+//   });
+//   setBoxes(updatedValue);
+// };
 
-  //  const [count, setCount] = useState(0);
+//  const [count, setCount] = useState(0);
--- src/Components/Counters/Counters.js
+++ src/Components/Counters/Counters.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import styles from "./Counters.module.css"
+import React from "react";
+import styles from "./Counters.module.css";
 
-const Counters = ({box}) => {
+const Counters = ({ box, boxes, setBoxes, isX, setIsX }) => {
+  //   const updatedValue = [...boxes].map((box) => {
+  //     if (box.id === e.target.id) {
+  //       if (count % 2 === 0) {
+  //         box.value = "0";
+  //       } else {
+  //         box.value = "X";
+  //       }
+  //     }
+  //     setCount(count + 1);
+  //     return box;
+  //   });
+  //   setBoxes(updatedValue);
+  const handleCounterClick = (e) => {
+    const updatedValue = [...boxes].map((box) => {
+      if (box.id === e.target.id) {
+        if (isX === true) {
+          box.value = "0";
+        } else {
+          box.value = "X";
+        }
+      }
+      setIsX(!isX);
+      return box;
+    });
+    console.log({ updatedValue });
+    console.log({isX})
+    setBoxes(updatedValue);
+};
+
+  //  const [count, setCount] = useState(0);
+
   return (
-    <div className={styles.box}>
-        <div className={styles.counter}>X</div>
+    <div className={styles.box} id={box.id} onClick={handleCounterClick}>
+      <div className={styles.counter} id={box.id}>
+        {box.value}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counters
+export default Counters;

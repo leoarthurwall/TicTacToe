25998dfb1cda5bf72688cdac3ac135f4c5a4048a
leoarthurwall <leowall93@gmail.com>
2023-01-19 22:39:42 +0000

    added player container

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from "./Player.module.css"
 
 const Player = () => {
   return (
-    <div>Player</div>
+    <div className={styles.container}>Player</div>
   )
 }
 

58d09d3f74296d8514dd6bd91277247185e0594c
leoarthurwall <leowall93@gmail.com>
2023-01-20 09:22:41 +0000

    re structured the turn content

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -4,7 +4,7 @@ import styles from "./Player.module.css";
 const Player = ({ isX }) => {
   return (
     <div className={styles.container}>
-      <h4 className={styles.name}> Next Turn: <span className={styles.span}>{isX ? "X" : "0"}</span> </h4>
+      <h4 className={styles.name}> <span className={styles.span}>{isX ? "X" : "0"}</span> Turn </h4>
     </div>
   );
 };

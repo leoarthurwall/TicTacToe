759c30bbf2f04f0de8fa46cd0fe6109970b19a13
leoarthurwall <leowall93@gmail.com>
2023-01-22 20:24:07 +0000

    created a score card component can can be reused for all 3 cards

--- src/Components/Score/Score.js
+++ src/Components/Score/Score.js
@@ -4,9 +4,9 @@ import styles from "./Score.module.css";
 const Score = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.button} style={{backgroundColor: "#61c1ba"}}>Score</div>
-      <div className={styles.button} style={{backgroundColor: "#8E9DA5"}}>Score</div>
-      <div className={styles.button} style={{backgroundColor: "#C9A963"}}>Score</div>
+      <div className={styles.button} style={{backgroundColor: "#61c1ba"}}>X (YOU)</div>
+      <div className={styles.button} style={{backgroundColor: "#8E9DA5"}}>TIES</div>
+      <div className={styles.button} style={{backgroundColor: "#C9A963"}}>O (CPU)</div>
     </section>
   );
 };
---
+++ src/Components/Score/ScoreCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Score.module.css"
+
+const ScoreCard = () => {
+  return (
+    <div>ScoreCard</div>
+  )
+}
+
+export default ScoreCard

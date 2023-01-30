97082550f2962aabf75f6987ac403a9810306ab6
leoarthurwall <leowall93@gmail.com>
2023-01-30 21:36:26 +0100

    created basic style for reset button

---
+++ src/Components/Reset/Reset.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Reset.module.css"
+
+const Reset = () => {
+  return (
+    <div className={styles.container}>Reset</div>
+  )
+}
+
+export default Reset

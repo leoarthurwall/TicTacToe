e128cb0b80b71ef844de4c229f31a0ec9239ef7d
leoarthurwall <leowall93@gmail.com>
2023-01-31 11:11:35 +0100

    aligned the top componments correctly using grid. this enabled the center player component to remain central despite the left and right component being different widths

--- src/Components/Reset/Reset.js
+++ src/Components/Reset/Reset.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import styles from "./Reset.module.css"
-import { BsArrowClockwise } from "react-icons/bs"
+import React from "react";
+import styles from "./Reset.module.css";
+import { BsArrowClockwise } from "react-icons/bs";
 
 const Reset = () => {
   return (
-    <div className={styles.container}>
-        <BsArrowClockwise size={30} color="black"/>
+    <div className={styles.gridWrapper}>
+      <div className={styles.container}>
+        <BsArrowClockwise size={30} color="black" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reset
+export default Reset;
